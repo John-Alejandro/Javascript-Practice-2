44160f8cf05887c7cfa38996aac48b3b9ecd00aa
john alejandro <john.e.alejandro01@gmail.com>
2022-04-27 22:30:51 -0500

    object literal getter example

--- js/class.js
+++ js/class.js
@@ -1,8 +1,45 @@
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+//
+// let person = new Person("John");
+// console.log(person.name);
+
+
 class Person {
     constructor(name) {
-        this.name = name;
+        this.setName(name);
+    }
+    getName() {
+        return this.name;
+    }
+    setName(newName) {
+        newName = newName.trim();
+        if (newName === '') {
+            throw 'The name cannot be empty';
+        }
+        this.name = newName;
     }
 }
 
-let person = new Person("John");
-console.log(person.name);
+let person = new Person('Jane Doe');
+console.log(person);
+
+
+let meeting = {
+    attendees: [],
+    add(attendee) {
+        console.log(`${attendee} joined the meeting.`);
+        this.attendees.push(attendee);
+        return this;
+    },
+    get latest() {
+        let count = this.attendees.length;
+        return count == 0 ? undefined : this.attendees[count - 1];
+    }
+};
+
+meeting.add('John').add('Jane').add('Peter');
+console.log(`The latest attendee is ${meeting.latest}.`);
