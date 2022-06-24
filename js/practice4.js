0f5df934c8fce6a1f19bd3c958fa524c7e08dab4
john alejandro <john.e.alejandro01@gmail.com>
2022-06-23 23:10:31 -0500

    moved to js directory

---
+++ js/practice4.js
@@ -0,0 +1,23 @@
+"use strict"
+const search = document.getElementById('search');
+
+search.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+});
+
+const movies = [
+    {name: "TOP GUN"},
+    {name: "TOP GUN: MAVERICK"},
+    {name: "MISSION IMPOSSIBLE"},
+    {name: "M.I.-2"},
+    {name: "M.I.-3"},
+    {name: "M.I.: GHOST PROTOCOL"},
+    {name: "M.I.: ROGUE NATION"},
+    {name: "M.I: FALLOUT"},
+    {name: "JERRY MAGUIRE"},
+    {name: "RISKY BUSINESS"},
+    {name: "JACK REACHER"},
+    {name: "WAR OF THE WORLDS"},
+];
+
+console.log(movies);
